eb303756a2763214befa45b221bf01f4e676c63d
Gonzalo Galeano <gozpellz@gmail.com>
2023-10-28 16:24:22 -0300

    configurando mainActivity

--- app/src/main/java/com/mariaserrana/myapplication/MainActivity.kt
+++ app/src/main/java/com/mariaserrana/myapplication/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.mariaserrana.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.mariaserrana.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        //Agregar animaciones
+        val animacion1 = AnimationUtils.loadAnimation(this, R.anim.arriba)
+
+        binding.imageView.animation = animacion1
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
     }
 }
